bbb95bb0d1c4e20a6b1801e21c9bc425da1e62cf
(jongin) <(whddls981203@gmail.com)>
2022-08-21 02:23:29 +0900

    2022.08.21 COMMIT-1

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override'); // PUT, DELETE를 사용하기 위한 method-override
 const Campground = require('./models/campground');
 
@@ -15,6 +16,7 @@ db.once('open', () => {
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
--- seeds/index.js
+++ seeds/index.js
@@ -23,9 +23,14 @@ const seedDB = async () => {
     // 캠핑장 랜덤으로 50개 만들기
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://images.unsplash.com/photo-1518602164578-cd0074062767?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius ratione fugiat modi officia quod ut et fuga unde molestiae ex tempore dolorem deleniti facere ipsa nemo, tempora eveniet! Magni, rem?',
+            price
         });
         await camp.save();
     }
