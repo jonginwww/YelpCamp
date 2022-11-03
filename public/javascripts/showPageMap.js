26ba6f39601174a3b37de04444e1b9b38af00c87
JonginWon <whddls981203@gmail.com>
2022-11-03 19:17:56 +0900

    지도 팝업 추가

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -10,4 +10,7 @@ map.on('style.load', () => {
     map.setFog({}); // Set the default atmosphere style
 });
 
-new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
+new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .setPopup(new mapboxgl.Popup({offset: 25}).setHTML(`<h3>${campground.title}</h3><p>${campground.location}</p>`))
+    .addTo(map);
